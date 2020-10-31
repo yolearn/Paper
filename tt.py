92be9c929ca5ee8608891deb514ab396c80410a5
yolearn <chad840901@gmail.com>
2020-10-31 10:15:09 +0800

    rm test

--- tt.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-embedding = nn.Embedding(10, 3)
-input = torch.zeros(2, 4, dtype=torch.long)
-output = embedding(input)
-print(input.size())
-print(output.size())
